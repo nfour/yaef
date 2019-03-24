a3fb55188920c0642a12036a1a55bda689d7014a
nfour <novus.nfour@gmail.com>
2019-03-24 20:27:01 +1000

    wip

--- README.md
+++ README.md
@@ -1,13 +1,40 @@
 # strict-events
 
+## reaco
 
-## To solve:
+> reaco is a project that only serves as a base to argue for event arch'ed frameworks and what the difference is/lessons to be learned.
 
-- Components do not define events as strings
+### Things that reaco got wrong:
+
+- Components define events as strings.
+  - They are typed - but are not completely enforced,
+    and are difficult to maintain (3 places)
+- Components connect wildly between each other
+  - Although this is a more flexible implementation, it means one can not track event propagation between components in a clear manner
+- 
+### Possible improvements:
+
 - Events are defined as interfaces
-  - These interfaces should define the call signature of `component.emit` or `component.on` and solve the subscriptions/declarations
+  - These interfaces, likely implemented as Classes, should define the call signature of the messages and a serializable identifier to facilitate message passing
 - Components connect via containers instead of each component being glued to each other
-  
+- Components, on construction, define the events they consume and publish, through dependency injection
+
+With that said, we could just do something entirely different to meet the goals.
+
+## Goals:
+
+- Produce new iteration of an evented framework
+- Must be simple in both api and concept, within the limits of soundness
+- Must not have complex rules or logic, only glue
+- Must provide support for arbitrary event passing layers, eg. remote components, IPC etc. ensuring event lifecycles are easy to reason about
+
 ## Research
 
 - Entity component system (eg. unreal engine)
+  - https://github.com/Xan0C/curbl-ecs#readme
+    - New project, based on ECS
+    - Typescript
+    - Dependency injection
+    - Classes & decorators
+  - https://github.com/ianpaschal/aurora#readme
+    - Game engine basis
---
+++ example.ts
@@ -0,0 +1,108 @@
+interface IEvent {
+  message: unknown
+  reply?: unknown
+}
+
+class Component<E extends {
+  sub: IEvent;
+  pub: IEvent;
+}> {
+  _sub!: E['sub']
+  _pub!: E['pub']
+
+  sub!: <Es extends E['sub']> (event: Es, cb: (arg: Es['message']) => Promise<Es['reply']>) => this
+  pub!: <Es extends E['pub']> (event: Es) => Promise<Es['reply']>
+
+}
+class EventContainer<E extends IEvent> {
+  _components!: Component<{ sub: E, pub: E }>;
+
+  connect(component: this['_components']) {}
+  sub!: <Es extends E> (event: Es, cb: (arg: Es['message']) => Promise<Es['reply']>) => this
+  pub!: <Es extends E> (event: Es) => Promise<Es['reply']>
+}
+
+class HttpRequestEvent implements IEvent {
+  message!: { method: string, resource: string }
+
+  /** In this example, all HttpRequestEvent's must be replied to with a the shape of a HttpResponseEvent */
+  reply!: HttpResponseEvent
+}
+
+class PostHttpRequestEvent implements HttpRequestEvent {
+  message!: { method: "POST", resource: string, body: string }
+  reply!: HttpResponseEvent
+
+}
+
+class GetHttpRequestEvent implements HttpRequestEvent {
+  message!: { method: "GET", resource: string }
+  reply!: HttpResponseEvent
+}
+
+class HttpServer extends Component<{
+  sub: HttpResponseEvent
+  pub: GetHttpRequestEvent | PostHttpRequestEvent,
+}> {}
+
+class RestApi extends Component<{
+  sub: PostHttpRequestEvent | GetHttpRequestEvent // Join them together here so that it is a subset instead of superset comparison
+  // This can be done better with a helper type during the construction of Component, preferebly using unions so to preserver discrimination in TS
+  pub: HttpResponseEvent
+}> {}
+
+class HttpResponseEvent implements IEvent {
+  message!: { body: string, headers: { 'content-length': number } }
+}
+
+const httpServer = new HttpServer();
+const restApi = new RestApi()
+
+// Pass. All events for each component are subset of either  HttpRequestEvent or HttpResponseEvent
+const httpEvents = new EventContainer<HttpRequestEvent|HttpResponseEvent>()
+
+httpEvents.connect(httpServer)
+httpEvents.connect(restApi)
+
+// Failure, only http request events allowed. No components can be connected.
+const httpRequestEvents = new EventContainer<HttpRequestEvent>()
+
+httpRequestEvents.connect(httpServer)
+httpRequestEvents.connect(restApi)
+
+// Failure. With a better type inferrence strategy we may make this work
+const getHttpEventResponder = new EventContainer<GetHttpRequestEvent|HttpResponseEvent>()
+
+getHttpEventResponder.connect(httpServer)
+getHttpEventResponder.connect(restApi)
+
+
+void (async () => {
+  const responseEvent = {
+    message: { headers: {'content-length': 3 }, body: 'bar' }
+  } as const;
+  
+  const fooBarEvent = {
+    message: { method: 'GET', resource: '/foo' },
+    reply: responseEvent
+  } as const
+  
+  restApi.sub(fooBarEvent, async ({ method, resource }) => {
+    const isGet: typeof method = 'GET'
+    const isFooxx: typeof resource = '/fooxx'
+    const isPost: typeof method = 'POST'
+  
+    // return { message: { body: 'baz', headers: { "content-length": 3 } } }
+    return responseEvent
+  });
+
+  const result = await httpEvents.pub(fooBarEvent)
+
+  result.message.body === 'bar'
+  result.message.headers['content-length'] === 3;
+
+  result.message.body === 'bars';
+  result.message.headers['content-length'] === 4;
+})();
+
+
