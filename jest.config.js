d36f9f17d0f0db59311093c29c89d1ba9dbc85b2
nfour <novus.nfour@gmail.com>
2019-03-31 15:54:18 +1000

    Fix tests

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
+/** @ts-check @type jest.ProjectConfig */
 module.exports = {
+  testPathIgnorePatterns: ['build'],
+  testMatch: ["**/*.test.ts"],
   preset: 'ts-jest',
   testEnvironment: 'node',
 };
