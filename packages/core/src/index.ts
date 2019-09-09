be920370a64d7e6dceff37d96fb9cc69819d8aad
nfour <novus.nfour@gmail.com>
2019-09-09 15:19:16 +1000

    w

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,5 +3,6 @@ export * from './types';
 export * from './componentry';
 export * from './registry';
 export * from './remote/remote';
+export * from './remote/types';
 export * from './debug';
 export { EventAwaiter } from './lib';
